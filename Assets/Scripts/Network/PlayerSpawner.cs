1d46d0897659869e11a0cd838929843b9669f588
Ashutosh Sharma <86849270+aashu1208@users.noreply.github.com>
2026-02-21 16:11:33 +0530

    addressables implemented and working

--- Assets/Scripts/Network/PlayerSpawner.cs
+++ Assets/Scripts/Network/PlayerSpawner.cs
@@ -1,9 +1,13 @@
 using UnityEngine;
 using Fusion;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
 
 public class PlayerSpawner : SimulationBehaviour, IPlayerJoined
 {
     public GameObject playerPrefab;
+    [SerializeField]
+    private string playerAddress = "Assets/Prefabs/Player.prefab";
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -20,7 +24,32 @@ public class PlayerSpawner : SimulationBehaviour, IPlayerJoined
     {
         if (player == Runner.LocalPlayer)
         {
-            Runner.Spawn(playerPrefab, Vector3.zero, Quaternion.identity, player);
+            //Runner.Spawn(playerPrefab, Vector3.zero, Quaternion.identity, player);
+            LoadAndSpawnPlayer(player);
+        }
+    }
+
+    private async void LoadAndSpawnPlayer(PlayerRef player)
+    {
+
+        AsyncOperationHandle<GameObject> handle = 
+        Addressables.LoadAssetAsync<GameObject>(playerAddress);
+
+        await handle.Task;
+
+        if(handle.Status == AsyncOperationStatus.Succeeded)
+        {
+
+            Runner.Spawn(handle.Result, Vector3.zero, Quaternion.identity, player);
+            Debug.Log("Player Spawned from Addressables successfully!");
+
+        }
+
+        else
+        {
+
+            Debug.LogError("Failed to load player prefab from Addressables: "+
+            handle.OperationException);
         }
     }
 }
